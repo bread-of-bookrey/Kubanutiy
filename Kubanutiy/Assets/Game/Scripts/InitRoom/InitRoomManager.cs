fba424bbe05b2e517192e2b377b3e65ca8bf0b3f
b00krey <tacticooler@yandex.ru>
2026-05-23 10:11:01 +0300

    Main Rooms added

---
+++ Kubanutiy/Assets/Game/Scripts/InitRoom/InitRoomManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InitRoomManager : MonoBehaviour
+{
+    void Start()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    void Update()
+    {
+        
+    }
+}
